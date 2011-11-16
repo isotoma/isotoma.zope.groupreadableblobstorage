249dc8a0f1bba0394f9bc7966cc5c668640d46dd
John Carr <john.carr@isotoma.com>
2011-11-16 13:42:46 +0000

    Make data group readable

--- isotoma/zope/groupreadableblobstorage/__init__.py
+++ isotoma/zope/groupreadableblobstorage/__init__.py
@@ -4,8 +4,11 @@ http://stackoverflow.com/questions/6168566/collective-xsendfile-zodb-blobs-and-u
 """
 
 import os
+import tempfile
+import weakref
+
 from ZODB import utils
-from ZODB.blob import FilesystemHelper
+from ZODB.blob import FilesystemHelper, BlobStorageMixin, Blob, is_blob_record
 from ZODB.blob import log, LAYOUT_MARKER
 
 def create(self):
@@ -60,7 +63,76 @@ def getPathForOID(self, oid, create=False):
             assert os.path.exists(path)
     return path
 
+def blob_mkstemp(self, oid, tid):
+    """Given an oid and a tid, return a temporary file descriptor
+    and a related filename.
+
+    The file is guaranteed to exist on the same partition as committed
+    data, which is important for being able to rename the file without a
+    copy operation.  The directory in which the file will be placed, which
+    is the return value of self.getPathForOID(oid), must exist before this
+    method may be called successfully.
+    """
+    oidpath = self.getPathForOID(oid)
+    fd, name = tempfile.mkstemp(suffix='.tmp', prefix=utils.tid_repr(tid),
+                                dir=oidpath)
+    os.chmod(name, 0640)
+    return fd, name
+
 FilesystemHelper.create = create
 FilesystemHelper.isSecure = isSecure
 FilesystemHelper.getPathForOID = getPathForOID
+FilesystemHelper.blob_mkstemp = blob_mkstemp
+
+
+
+def copyTransactionsFrom(self, other):
+    for trans in other.iterator():
+        self.tpc_begin(trans, trans.tid, trans.status)
+        for record in trans:
+            blobfilename = None
+            if is_blob_record(record.data):
+                try:
+                    blobfilename = other.loadBlob(record.oid, record.tid)
+                except POSKeyError:
+                    pass
+            if blobfilename is not None:
+                fd, name = tempfile.mkstemp(
+                    suffix='.tmp', dir=self.fshelper.temp_dir)
+                os.close(fd)
+                os.chmod(name, 0640)
+                utils.cp(open(blobfilename, 'rb'), open(name, 'wb'))
+                self.restoreBlob(record.oid, record.tid, record.data,
+                                 name, record.data_txn, trans)
+            else:
+                self.restore(record.oid, record.tid, record.data,
+                             '', record.data_txn, trans)
+
+        self.tpc_vote(trans)
+        self.tpc_finish(trans)
+
+BlobStorageMixin.copyTransactionsFrom = copyTransactionsFrom
+
+
+
+
+def _create_uncommitted_file(self):
+    assert self._p_blob_uncommitted is None, (
+        "Uncommitted file already exists.")
+    if self._p_jar:
+        tempdir = self._p_jar.db()._storage.temporaryDirectory()
+    else:
+        tempdir = tempfile.gettempdir()
+    filename = utils.mktemp(dir=tempdir)
+    os.chmod(filename, 0640)
+    self._p_blob_uncommitted = filename
+
+    def cleanup(ref):
+        if os.path.exists(filename):
+            os.remove(filename)
+
+    self._p_blob_ref = weakref.ref(self, cleanup)
+    return filename
+
+Blob._create_uncommitted_file = _create_uncommitted_file
 
